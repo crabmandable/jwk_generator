6c125556559d10e59b3cfd865dd45a7867dec587
Crabmandable <zach.gerstman@gmail.com>
2022-09-26 13:03:52 +0300

    Made it work with openssl 1.1.1 too

--- includes/jwk_generator/jwk_generator.hpp
+++ includes/jwk_generator/jwk_generator.hpp
@@ -1,25 +1,29 @@
 #pragma once
 
-#include <openssl/types.h>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include "jwk_generator/uuid.hpp"
-#include <openssl/rsa.h>
-#include <openssl/ec.h>
-#include <openssl/ecdsa.h>
-#include "openssl/evp.h"
 #include <memory>
 #include <tuple>
 #include <stdint.h>
 #include <openssl/err.h>
-#include <openssl/core_names.h>
 #include <openssl/bio.h>
 #include <openssl/pem.h>
+#include <openssl/rsa.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include "openssl/evp.h"
 
 #include "jwk_generator/base64_url.hpp"
 #include "jwk_generator/uuid.hpp"
 #include "jwk_generator/json.hpp"
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L // 3.0.0
+#define JWKGEN_OPENSSL_3_0
+#include <openssl/types.h>
+#include <openssl/core_names.h>
+#endif
 
 namespace jwk_generator {
     namespace detail {
@@ -30,6 +34,7 @@ namespace jwk_generator {
             return std::string(errStr);
         }
 
+#ifdef JWKGEN_OPENSSL_3_0
         static inline const char* ECDSABitsToCurve(size_t nBits) {
             switch(nBits) {
                 case 256: {
@@ -44,10 +49,32 @@ namespace jwk_generator {
             }
             throw std::runtime_error("Unsupported ecdsa algorithm");
         }
+#else
+        static inline int ECDSABitsToCurve(size_t nBits) {
+            switch(nBits) {
+                case 256: {
+                    return NID_X9_62_prime256v1;
+                }
+                case 512: {
+                    return NID_secp521r1;
+                }
+                case 384: {
+                    return NID_secp384r1;
+                }
+            }
+            throw std::runtime_error("Unsupported ecdsa algorithm");
+        }
+#endif
+    };
+
+    struct openssl_error: public std::runtime_error {
+        openssl_error(std::string what) : std::runtime_error(what + detail::OpenSSLLastError()) {}
     };
 
     template<size_t shaBits>
     struct RSAKey {
+        public:
+        static constexpr const size_t nBits = 2048;
         std::shared_ptr<EVP_PKEY> keyPair;
         std::string modulous;
         std::string exponent;
@@ -55,39 +82,58 @@ namespace jwk_generator {
         RSAKey() {
             using namespace detail;
 
-            size_t len;
-            keyPair = {EVP_RSA_gen(2048), EVP_PKEY_free};
+#ifdef JWKGEN_OPENSSL_3_0
+            keyPair = {EVP_RSA_gen(nBits), EVP_PKEY_free};
             if (!keyPair) {
-                throw std::runtime_error(std::string("Unable to generate rsa key: ") + OpenSSLLastError());
-            }
-            auto modBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-            if (!modBN) {
-                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-            }
-            BIGNUM* modTmp = modBN.get();
-            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_N, &modTmp)) {
-                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
+                throw openssl_error("Unable to generate rsa key: ");
             }
 
-            len = BN_num_bytes(modBN.get());
+            BIGNUM* modBN = nullptr;
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_N, &modBN)) {
+                throw openssl_error("Unable to retrieve public key: ");
+            }
+
+            BIGNUM* exBN = nullptr;
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_E, &exBN)) {
+                throw openssl_error("Unable to retrieve public key: ");
+            }
+#else
+            std::shared_ptr<BIGNUM> exShared = {BN_new(), BN_free};
+            if (!exShared) {
+                throw openssl_error("Unable to allocate BN: ");
+            }
+            BIGNUM* exBN = exShared.get();
+            BN_set_word(exBN, 65537);
+
+            // rsa becomes owned by the evp, so don't wrap in smart pointer
+            RSA* rsa = RSA_new();
+            if (!RSA_generate_key_ex(rsa, nBits, exBN, NULL)) {
+                RSA_free(rsa);
+                throw openssl_error("Unable to generate rsa key: ");
+            }
+
+            keyPair = {EVP_PKEY_new(), EVP_PKEY_free};
+            if (!keyPair) {
+                RSA_free(rsa);
+                throw openssl_error("Unable to generate rsa key: ");
+            }
+            EVP_PKEY* pkey = keyPair.get();
+            if (!EVP_PKEY_assign_RSA(pkey, rsa)) {
+                throw openssl_error("Unable to generate rsa key: ");
+            }
+            const BIGNUM* modBN = RSA_get0_n(rsa);
+#endif
+
+            size_t len = BN_num_bytes(modBN);
             std::vector<uint8_t> modBin;
             modBin.resize(len);
-            BN_bn2bin(modBN.get(), modBin.data());
+            BN_bn2bin(modBN, modBin.data());
             modulous = base64_url_encode(modBin);
 
-            auto exBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-            if (!exBN) {
-                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-            }
-            BIGNUM* exTmp = exBN.get();
-            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_E, &exTmp)) {
-                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-            }
-
-            len = BN_num_bytes(exBN.get());
+            len = BN_num_bytes(exBN);
             std::vector<uint8_t> exBin;
             exBin.resize(len);
-            BN_bn2bin(exBN.get(), exBin.data());
+            BN_bn2bin(exBN, exBin.data());
             exponent = base64_url_encode(exBin);
         }
 
@@ -108,39 +154,70 @@ namespace jwk_generator {
         ECDSAKey() {
             using namespace detail;
 
-            size_t len;
+#ifdef JWKGEN_OPENSSL_3_0
             keyPair = {EVP_EC_gen(ECDSABitsToCurve(nBits)), EVP_PKEY_free};
             if (!keyPair) {
-                throw std::runtime_error(std::string("Unable to generate ec key: ") + OpenSSLLastError());
+                throw openssl_error("Unable to generate ec key: ");
             }
-            auto xBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-            if (!xBN) {
-                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-            }
-            BIGNUM* xTmp = xBN.get();
-            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_X, &xTmp)) {
-                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
+            BIGNUM* xBN = nullptr;
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_X, &xBN)) {
+                throw openssl_error("Unable to extract coordinates key: ");
             }
 
-            len = BN_num_bytes(xBN.get());
+            BIGNUM* yBN = nullptr;
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_Y, &yBN)) {
+                throw openssl_error("Unable extract coordinates from key: ");
+            }
+#else
+            // don't wrap in smart pointer because the EVP own it
+            std::shared_ptr<EC_GROUP> groupShared = {EC_GROUP_new_by_curve_name(ECDSABitsToCurve(nBits)), EC_GROUP_free};
+            const EC_GROUP* group = groupShared.get();
+
+            EC_KEY* ec = EC_KEY_new();
+            if (!EC_KEY_set_group(ec, group)) {
+                EC_KEY_free(ec);
+                throw openssl_error("Unable to generate ec key: ");
+            }
+            if (!EC_KEY_generate_key(ec)) {
+                EC_KEY_free(ec);
+                throw openssl_error("Unable to generate ec key: ");
+            }
+            keyPair = {EVP_PKEY_new(), EVP_PKEY_free};
+            if (!keyPair) {
+                EC_KEY_free(ec);
+                throw openssl_error("Unable to generate ec key: ");
+            }
+            EVP_PKEY* pkey = keyPair.get();
+            if (!EVP_PKEY_assign_EC_KEY(pkey, ec)) {
+                throw openssl_error("Unable to generate ec key: ");
+            }
+
+            std::shared_ptr<BIGNUM> xShared = {BN_new(), BN_free};
+            BIGNUM* xBN = xShared.get();
+            if (!xBN) {
+                throw openssl_error("Unable to allocate BN: ");
+            }
+            std::shared_ptr<BIGNUM> yShared = {BN_new(), BN_free};
+            BIGNUM* yBN = yShared.get();
+            if (!yBN) {
+                throw openssl_error("Unable to allocate BN: ");
+            }
+            auto point = EC_KEY_get0_public_key(ec);
+            if (!EC_POINT_get_affine_coordinates(group, point, xBN, yBN, NULL)) {
+                throw openssl_error("Unable to extract coordinates from key: ");
+            }
+#endif
+
+            size_t len = BN_num_bytes(xBN);
             std::vector<uint8_t> xBin;
             xBin.resize(len);
-            BN_bn2bin(xBN.get(), xBin.data());
+            BN_bn2bin(xBN, xBin.data());
             pointX = base64_url_encode(xBin);
 
-            auto yBN = std::shared_ptr<BIGNUM>(BN_new(), BN_free);
-            if (!yBN) {
-                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-            }
-            BIGNUM* yTmp = yBN.get();
-            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_Y, &yTmp)) {
-                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-            }
-
-            len = BN_num_bytes(yBN.get());
+            len = BN_num_bytes(yBN);
             std::vector<uint8_t> yBin;
             yBin.resize(len);
-            BN_bn2bin(yBN.get(), yBin.data());
+            BN_bn2bin(yBN, yBin.data());
             pointY = base64_url_encode(yBin);
         }
 
@@ -162,17 +239,17 @@ namespace jwk_generator {
             using namespace detail;
             auto pemKeyBIO = std::shared_ptr<BIO>(BIO_new(BIO_s_secmem()), BIO_free);
             if (!pemKeyBIO) {
-                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
+                throw openssl_error("Unable to retrieve public key: ");
             }
             EVP_PKEY* tmpEVP = key.keyPair.get();
             int result = writeKeyToBIO(pemKeyBIO.get(), tmpEVP);
             if (!result) {
-                throw std::runtime_error(std::string("Unable to convert key to pem: ") + OpenSSLLastError());
+                throw openssl_error("Unable to convert key to pem: ");
             }
             char* buffer;
             auto len = BIO_get_mem_data(pemKeyBIO.get(), &buffer);
             if (!len) {
-                throw std::runtime_error(std::string("Unable to retrieve key from bio: ") + OpenSSLLastError());
+                throw openssl_error("Unable to retrieve key from bio: ");
             }
             std::string pem;
             pem.resize(len);
