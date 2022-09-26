dae2e4363114e7fa41a05e55248fdbff3ae8e4f2
Crabmandable <zach.gerstman@gmail.com>
2022-09-26 10:11:10 +0300

    Tidying up
    Packed everything so the directory can simply be included

--- includes/jwk_generator/jwk_generator.hpp
+++ includes/jwk_generator/jwk_generator.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "nlohmann/json.hpp"
 #include <openssl/types.h>
 #include <sstream>
 #include <stdexcept>
@@ -17,8 +16,10 @@
 #include <openssl/core_names.h>
 #include <openssl/bio.h>
 #include <openssl/pem.h>
-#include "cppcodec/base64_url_unpadded.hpp"
-#include "uuid.hpp"
+
+#include "jwk_generator/base64_url.hpp"
+#include "jwk_generator/uuid.hpp"
+#include "jwk_generator/json.hpp"
 
 namespace jwk_generator {
     namespace detail {
@@ -45,112 +46,112 @@ namespace jwk_generator {
         }
     };
 
+    template<size_t shaBits>
+    struct RSAKey {
+        std::shared_ptr<EVP_PKEY> keyPair;
+        std::string modulous;
+        std::string exponent;
+
+        RSAKey() {
+            using namespace detail;
+
+            size_t len;
+            keyPair = {EVP_RSA_gen(2048), EVP_PKEY_free};
+            if (!keyPair) {
+                throw std::runtime_error(std::string("Unable to generate rsa key: ") + OpenSSLLastError());
+            }
+            auto modBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
+            if (!modBN) {
+                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
+            }
+            BIGNUM* modTmp = modBN.get();
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_N, &modTmp)) {
+                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
+            }
+
+            len = BN_num_bytes(modBN.get());
+            std::vector<uint8_t> modBin;
+            modBin.resize(len);
+            BN_bn2bin(modBN.get(), modBin.data());
+            modulous = base64_url_encode(modBin);
+
+            auto exBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
+            if (!exBN) {
+                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
+            }
+            BIGNUM* exTmp = exBN.get();
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_E, &exTmp)) {
+                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
+            }
+
+            len = BN_num_bytes(exBN.get());
+            std::vector<uint8_t> exBin;
+            exBin.resize(len);
+            BN_bn2bin(exBN.get(), exBin.data());
+            exponent = base64_url_encode(exBin);
+        }
+
+        void insertJson(nlohmann::json& json) const {
+            json["alg"] = "RS" + std::to_string(shaBits);
+            json["kty"] = "RSA";
+            json["e"] = exponent;
+            json["n"] = modulous;
+        }
+    };
+
     template<size_t nBits>
-        struct RSAKey {
-            std::shared_ptr<EVP_PKEY> keyPair;
-            std::string modulous;
-            std::string exponent;
+    struct ECDSAKey {
+        std::shared_ptr<EVP_PKEY> keyPair;
+        std::string pointX;
+        std::string pointY;
 
-            RSAKey() {
-                using namespace detail;
+        ECDSAKey() {
+            using namespace detail;
 
-                size_t len;
-                keyPair = {EVP_RSA_gen(nBits), EVP_PKEY_free};
-                if (!keyPair) {
-                    throw std::runtime_error(std::string("Unable to generate rsa key: ") + OpenSSLLastError());
-                }
-                auto modBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-                if (!modBN) {
-                    throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-                }
-                BIGNUM* modTmp = modBN.get();
-                if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_N, &modTmp)) {
-                    throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-                }
-
-                len = BN_num_bytes(modBN.get());
-                std::vector<uint8_t> modBin;
-                modBin.resize(len);
-                BN_bn2bin(modBN.get(), modBin.data());
-                modulous = cppcodec::base64_url_unpadded::encode(modBin);
-
-                auto exBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-                if (!exBN) {
-                    throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-                }
-                BIGNUM* exTmp = exBN.get();
-                if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_RSA_E, &exTmp)) {
-                    throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-                }
-
-                len = BN_num_bytes(exBN.get());
-                std::vector<uint8_t> exBin;
-                exBin.resize(len);
-                BN_bn2bin(exBN.get(), exBin.data());
-                exponent = cppcodec::base64_url_unpadded::encode(exBin);
+            size_t len;
+            keyPair = {EVP_EC_gen(ECDSABitsToCurve(nBits)), EVP_PKEY_free};
+            if (!keyPair) {
+                throw std::runtime_error(std::string("Unable to generate ec key: ") + OpenSSLLastError());
+            }
+            auto xBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
+            if (!xBN) {
+                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
+            }
+            BIGNUM* xTmp = xBN.get();
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_X, &xTmp)) {
+                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
             }
 
-            void insertJson(nlohmann::json& json) const {
-                json["alg"] = "RS" + std::to_string(nBits);
-                json["kty"] = "RSA";
-                json["e"] = exponent;
-                json["n"] = modulous;
+            len = BN_num_bytes(xBN.get());
+            std::vector<uint8_t> xBin;
+            xBin.resize(len);
+            BN_bn2bin(xBN.get(), xBin.data());
+            pointX = base64_url_encode(xBin);
+
+            auto yBN = std::shared_ptr<BIGNUM>(BN_new(), BN_free);
+            if (!yBN) {
+                throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
             }
-        };
-
-    template<size_t nBits>
-        struct ECDSAKey {
-            std::shared_ptr<EVP_PKEY> keyPair;
-            std::string pointX;
-            std::string pointY;
-
-            ECDSAKey() {
-                using namespace detail;
-
-                size_t len;
-                keyPair = {EVP_EC_gen(ECDSABitsToCurve(nBits)), EVP_PKEY_free};
-                if (!keyPair) {
-                    throw std::runtime_error(std::string("Unable to generate ec key: ") + OpenSSLLastError());
-                }
-                auto xBN = std::shared_ptr<BIGNUM>{BN_new(), BN_free};
-                if (!xBN) {
-                    throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-                }
-                BIGNUM* xTmp = xBN.get();
-                if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_X, &xTmp)) {
-                    throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-                }
-
-                len = BN_num_bytes(xBN.get());
-                std::vector<uint8_t> xBin;
-                xBin.resize(len);
-                BN_bn2bin(xBN.get(), xBin.data());
-                pointX = cppcodec::base64_url_unpadded::encode(xBin);
-
-                auto yBN = std::shared_ptr<BIGNUM>(BN_new(), BN_free);
-                if (!yBN) {
-                    throw std::runtime_error(std::string("Unable to allocate BN") + OpenSSLLastError());
-                }
-                BIGNUM* yTmp = yBN.get();
-                if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_Y, &yTmp)) {
-                    throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
-                }
-
-                len = BN_num_bytes(yBN.get());
-                std::vector<uint8_t> yBin;
-                yBin.resize(len);
-                BN_bn2bin(yBN.get(), yBin.data());
-                pointY = cppcodec::base64_url_unpadded::encode(yBin);
+            BIGNUM* yTmp = yBN.get();
+            if (!EVP_PKEY_get_bn_param(keyPair.get(), OSSL_PKEY_PARAM_EC_PUB_Y, &yTmp)) {
+                throw std::runtime_error(std::string("Unable to retrieve public key: ") + OpenSSLLastError());
             }
 
-            void insertJson(nlohmann::json& json) const {
-                json["alg"] = "ES" + std::to_string(nBits);
-                json["kty"] = "EC";
-                json["x"] = pointX;
-                json["y"] = pointY;
-                json["crv"] = std::string("P-") + std::to_string(nBits);
-            }
-        };
+            len = BN_num_bytes(yBN.get());
+            std::vector<uint8_t> yBin;
+            yBin.resize(len);
+            BN_bn2bin(yBN.get(), yBin.data());
+            pointY = base64_url_encode(yBin);
+        }
+
+        void insertJson(nlohmann::json& json) const {
+            json["alg"] = "ES" + std::to_string(nBits);
+            json["kty"] = "EC";
+            json["x"] = pointX;
+            json["y"] = pointY;
+            json["crv"] = std::string("P-") + std::to_string(nBits);
+        }
+    };
 
     template <typename KeySpec>
     class JwkGenerator {
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -17,3 +17,45 @@ TEST_F(Test, Test1) {
     std::cout << jwks.get<1>().private_to_pem() << std::endl;
     std::cout << jwks.get<1>().public_to_pem() << std::endl;
 }
+
+TEST_F(Test, ECDSA256) {
+    auto jwk = JwkGenerator<ECDSAKey<256>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
+
+TEST_F(Test, ECDSA384) {
+    auto jwk = JwkGenerator<ECDSAKey<384>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
+
+TEST_F(Test, ECDSA512) {
+    auto jwk = JwkGenerator<ECDSAKey<512>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
+
+TEST_F(Test, RSA256) {
+    auto jwk = JwkGenerator<RSAKey<256>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
+
+TEST_F(Test, RSA384) {
+    auto jwk = JwkGenerator<RSAKey<384>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
+
+TEST_F(Test, RSA512) {
+    auto jwk = JwkGenerator<RSAKey<512>>();
+    std::cout << jwk << std::endl;
+    std::cout << jwk.private_to_pem() << std::endl;
+    std::cout << jwk.public_to_pem() << std::endl;
+}
